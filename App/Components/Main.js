2e5ba29f6dab743cae329e1014840ef9f87a7ab1
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-03-01 15:38:18 +0100

    show error and activity indicator in main component

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -98,6 +98,9 @@ export default class Main extends Component {
   }
 
   render () {
+    var showErr = (
+      this.state.error ? <Text>{this.state.error}</Text> : <View></View>
+    );
     return (
       <View style={styles.mainContainer}>
         <Text style={styles.title}>Search for a Github User</Text>
@@ -111,6 +114,12 @@ export default class Main extends Component {
           underlayColor='white'>
           <Text style={styles.buttonText}>SEARCH</Text>
         </TouchableHighlight>
+        <ActivityIndicator
+          animating={this.state.isLoading}
+          color="#111"
+          size="large">
+        </ActivityIndicator>
+        {showErr}
       </View>
     )
   }
